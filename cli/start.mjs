7a95280c25de061a3d726788b6d6f30457527a7a
igor.gulyaschy <gulyaschy@gmail.com>
2023-10-03 21:26:58 +0300

    initial

--- cli/start.mjs
+++ cli/start.mjs
@@ -1,30 +1,53 @@
-import { cpSync } from 'fs'
-import { execSync } from 'child_process'
+#!/usr/bin/env node
+import { cpSync, renameSync } from "fs";
+import { execSync } from "child_process";
 
-const LIBRARY_PATH = './node_modules/koa-inversify'
-const TEMPLATES_PATH = `${LIBRARY_PATH}/template`
+const LIBRARY_PATH = "./node_modules/starter-kit";
+const TEMPLATES_PATH = `${LIBRARY_PATH}/templates`;
 
-async function run() {
-  copyFiles()
-  updatePkgJson()
-  await installPackages()
+function run() {
+  copyFiles();
+  renameFiles();
+  updatePkgJson();
+  installPackages();
 }
 
-async function installPackages() {
-  const packages = ['inversify', 'reflect-metadata', 'koa-inversify']
-  const devPackages = ['typescript', 'ts-node', '@types/koa']
-  execSync(`npm i ${packages.join(' ')}`, { stdio: 'inherit' })
-  execSync(`npm i --save-dev ${devPackages.join(' ')}`, { stdio: 'inherit' })
+const ESLINT = [
+  "@typescript-eslint/eslint-plugin",
+  "eslint",
+  "eslint-config-standard-with-typescript",
+  "eslint-plugin-import",
+  "eslint-plugin-n",
+  "eslint-plugin-prettier",
+  "eslint-plugin-promise",
+];
+
+const PRETTIER = ["prettier"];
+
+const NODE = ["ts-node", "typescript", "dotenv"];
+
+function installPackages() {
+  console.log("Instaling packages");
+  const devPackages = [ESLINT, PRETTIER, NODE].flat();
+  execSync(`npm i --save-dev ${devPackages.join(" ")}`, { stdio: "inherit" });
 }
 
 function copyFiles() {
-  cpSync(TEMPLATES_PATH, process.cwd(), { recursive: true })
+  cpSync(TEMPLATES_PATH, process.cwd(), { recursive: true });
+}
+
+function renameFiles() {
+  console.log("Rename files");
+  const namesToRename = ["!.gitignore"];
+  namesToRename.forEach((name) => {
+    renameSync(`./${name}`, `./${name.slice(1)}`);
+  });
 }
 
 function updatePkgJson() {
   execSync(
-    'npm pkg set "scripts.start:dev"="node -r ts-node/register -r reflect-metadata ./src/index.ts"'
-  )
+    'npm pkg set "scripts.start:dev"="node -r ts-node/register -r dotenv/config  ./src/index.ts"'
+  );
 }
 
-run()
+run();
